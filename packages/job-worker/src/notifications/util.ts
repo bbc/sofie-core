d56de6cec185ba1a0d3eec34fa63b10c48135149
Julian Waller <git@julusian.co.uk>
2024-10-16 14:20:11 +0100

    wip: tidy

--- packages/job-worker/src/notifications/util.ts
+++ packages/job-worker/src/notifications/util.ts
@@ -2,31 +2,10 @@ import {
 	ITranslatableMessage,
 	wrapTranslatableMessageFromBlueprints,
 } from '@sofie-automation/corelib/dist/TranslatableMessage'
-import type { INotification, INotificationWithTarget, INotificationsModel } from './NotificationsModel'
+import type { INotification } from './NotificationsModel'
 import { INoteBase } from '@sofie-automation/corelib/dist/dataModel/Notes'
 import { BlueprintId } from '@sofie-automation/corelib/dist/dataModel/Ids'
 import { getHash } from '@sofie-automation/corelib/dist/hash'
-
-export async function replaceAllNotificationsForCategory(
-	model: INotificationsModel,
-	category: string,
-	newNotifications: INotificationWithTarget[]
-): Promise<void> {
-	const existingNotifications = await model.getAllNotifications(category)
-
-	// Remove any notifications that are no longer present
-	const newNotificationKeys = new Set(newNotifications.map((n) => n.id))
-	for (const notification of existingNotifications) {
-		if (!newNotificationKeys.has(notification.id)) {
-			model.clearNotification(category, notification.id)
-		}
-	}
-
-	// Add any new notifications
-	for (const notification of newNotifications) {
-		model.setNotification(category, notification)
-	}
-}
 
 /** Generate the translation for a string, to be applied later when it gets rendered */
 export function generateTranslation(
