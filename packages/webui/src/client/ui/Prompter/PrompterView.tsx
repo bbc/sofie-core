f24f755c86275e6f151d8973e5874bdf61a00404
olzzon <github@olzzon.dk>
2025-04-11 21:33:07 +0200

    fix: after bootstrap was added the scrollBy(x, y) was using smooth scroll. using scrollBy(top: y, behaviour: instant) solves that problem

--- packages/webui/src/client/ui/Prompter/PrompterView.tsx
+++ packages/webui/src/client/ui/Prompter/PrompterView.tsx
@@ -808,6 +808,7 @@ const PrompterContent = withTranslation()(
 
 					window.scrollBy({
 						top: top - scrollAnchor.offset,
+						behavior: 'instant',
 					})
 					// We've scrolled, exit the function!
 					return
@@ -819,6 +820,7 @@ const PrompterContent = withTranslation()(
 					// the css margins magically does it for us.
 					window.scrollBy({
 						top: top - readPosition,
+						behavior: 'instant',
 					})
 					// We've scrolled, exit the function!
 					return
