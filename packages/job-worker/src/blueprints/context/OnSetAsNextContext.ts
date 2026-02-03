2a54056eeb39da0a5eb6f668d296acc2b6b40843
Julian Waller <git@julusian.co.uk>
2026-02-03 16:27:59 +0000

    feat: allow t-timers from syncIngestChanges

--- packages/job-worker/src/blueprints/context/OnSetAsNextContext.ts
+++ packages/job-worker/src/blueprints/context/OnSetAsNextContext.ts
@@ -50,7 +50,7 @@ export class OnSetAsNextContext
 		public readonly manuallySelected: boolean
 	) {
 		super(contextInfo, context, showStyle, watchedPackages)
-		this.#tTimersService = new TTimersService(playoutModel)
+		this.#tTimersService = TTimersService.withPlayoutModel(playoutModel)
 	}
 
 	public get quickLoopInfo(): BlueprintQuickLookInfo | null {
