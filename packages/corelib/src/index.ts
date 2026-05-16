a14c7cebe4f98348115c9c8547d7721e4ed7a098
Julian Waller <julian@superfly.tv>
2026-05-16 11:27:07 +0100

    fix: enable rspack for meteor, to fix sofie launching with esm packages (#1747)

--- packages/corelib/src/index.ts
+++ packages/corelib/src/index.ts
@@ -1,4 +1,4 @@
 // Re-export to reduce dependency duplication
-export { Timecode } from 'timecode'
+export { Timecode } from './timecode.js'
 
 export { MOS } from '@sofie-automation/shared-lib/dist/mos'
